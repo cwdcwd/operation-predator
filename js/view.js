421c5cea4aa9aa799659ba33dcfe46537212048c
Cain Huang <countcainht@gmail.com>
2015-08-31 22:00:49 -0400

    fix issue #1 and adjust layout

--- js/view.js
+++ js/view.js
@@ -13,7 +13,7 @@
     var HIDE_CLASS          = 'hide',
         EDGE_LENGTH_OF_CELL = 117,
         MARGIN_OF_CELL      = 12,
-        HOT_FILL_MAPPING = {
+        HOT_FILL_MAPPING    = {
             1: '#EB6045',
             2: '#FDC171',
             3: '#D2EC9B',
@@ -71,6 +71,42 @@
         this._numberOfAllCells = n;
     };
 
+    ViewManager.prototype._drawLegend = function () {
+        var lengend = this._svg
+                .append('g'),
+            startY  = this._scale.height / 2 - 200,
+            startX  = (MARGIN_OF_CELL + EDGE_LENGTH_OF_CELL) * this._numberOfColumns + 45;
+
+        lengend.append('text')
+            .text('Hot')
+            .attr('x', startX)
+            .attr('y', startY)
+            .attr('text-anchor', 'middle')
+            .attr('style', 'font-size:22px;color:#333E48;');
+
+        for (var i = 1; i < 6; i++) {
+            lengend.append('rect')
+                .attr('width', 50)
+                .attr('height', 70)
+                .attr('fill', HOT_FILL_MAPPING[i])
+                .attr('x', startX - 25)
+                .attr('y', startY + 75 * (i - 1) + 20)
+                .attr('filter', 'url(#inset-shadow)');
+            lengend.append('text')
+                .text(i)
+                .attr('x', startX + 40)
+                .attr('y', startY + 75 * i - 10)
+                .attr('style', 'font-size:30px;color:#333E48;');
+        }
+        lengend.append('text')
+            .text('Cold')
+            .attr('x', startX)
+            .attr('y', startY + 425)
+            .attr('text-anchor', 'middle')
+            .attr('style', 'font-size:22px;color:#333E48;');
+        this._svg.attr('width', startX+70);
+    };
+
     /**
      * define the size of the map
      * @returns {ViewManager}
@@ -79,12 +115,15 @@
         //need to clean the whole svg
         this._init();
 
-        this._scale.width = $('.hot-map svg').width();
-        this._numberOfColumns = parseInt(this._scale.width / EDGE_LENGTH_OF_CELL);
+        this._scale.width = $('.hot-map svg').width() * 0.94;
+        this._numberOfColumns = Math.min(parseInt(this._scale.width / (MARGIN_OF_CELL + EDGE_LENGTH_OF_CELL)), this._numberOfAllCells);
         this._numberOfRows = Math.ceil(this._numberOfAllCells / this._numberOfColumns);
+        this._scale.height = Math.max((MARGIN_OF_CELL + EDGE_LENGTH_OF_CELL) * this._numberOfRows+20, 500);
 
         this._svg
-            .attr('height', (MARGIN_OF_CELL + EDGE_LENGTH_OF_CELL) * this._numberOfRows);
+            .attr('height', this._scale.height);
+        console.log(this._numberOfColumns);
+        this._drawLegend();
         return this;
     };
 
@@ -96,8 +135,8 @@
         for (i = 0; i < this._numberOfColumns; i++) {
             for (j = 0; j < this._numberOfRows; j++) {
                 this._cellPositions.push({
-                    x: i * (MARGIN_OF_CELL + EDGE_LENGTH_OF_CELL),
-                    y: j * (MARGIN_OF_CELL + EDGE_LENGTH_OF_CELL),
+                    x: i * (MARGIN_OF_CELL + EDGE_LENGTH_OF_CELL)+10,
+                    y: j * (MARGIN_OF_CELL + EDGE_LENGTH_OF_CELL)+10,
                     occupied: false
                 });
             }
